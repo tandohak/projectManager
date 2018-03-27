c6aa2c5c66b1a9565ad1e2185aa919379876d785
[Home]yoon pyo <ypyo09@naver.com>
2018-03-27 22:47:50 +0900

    구글 로그인 설정중...

--- src/main/java/com/dgit/controller/HomeController.java
+++ src/main/java/com/dgit/controller/HomeController.java
@@ -1,11 +1,10 @@
 package com.dgit.controller;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.social.google.connect.GoogleConnectionFactory;
+import org.springframework.social.oauth2.OAuth2Parameters;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,14 +12,19 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
-	
+	@Autowired
+	private GoogleConnectionFactory googleConnectionFactory;
+
+	@Autowired
+	private OAuth2Parameters googleOAuth2Parameters;
+
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
+
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Model model) {
 		logger.info("[home]");
-		
+
 		return "index";
-	}
-	
+	} 
+
 }
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -13,17 +13,24 @@
 	<div class="container-fluid">
 		<div id="loginBox">
 			<div id="loginformWrap">
-				<h2 class="tit_h2">로그인</h2>
+				<h2 class="tit_h2">로그인</h2>  
 				<form>
 					<input type="email" class="form-control" id="email"  
 						placeholder="이메일 주소"> <input type="password"  
 						class="form-control" id="pwd" placeholder="비밀번호">
 
 					<input type="submit" value="로그인">
-					
+					 
 					<span class="bar"></span> <span>또는</span> <span class="bar"></span>
 
-					<a href="#" class="btn">로그인</a> 
+					<a href="https://accounts.google.com/o/oauth2/v2/auth?
+ scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fdrive.metadata.readonly&
+ access_type=offline&
+ include_granted_scopes=true&
+ state=state_parameter_passthrough_value&  
+ redirect_uri=http%3A%2F%2Foauth2.example.com%2Fcallback&
+ response_type=code&
+ client_id=141501242517-c6pe1khepdp7tl4fcf03bifkk382s1s1.apps.googleusercontent.com" class="btn">구글로 로그인</a> 
 				</form>
 			</div>
 		</div>
